7a1c385bfb486b71e77aca159517981844b24270
unknown <RengifoVera>
2022-04-11 12:41:38 -0500

    Animations

--- src/componets/SobreMi.jsx
+++ src/componets/SobreMi.jsx
@@ -23,6 +23,7 @@ export function SobreMi() {
     AOS.init();
     AOS.refresh(); 
   }, []);
+    
   return (
     <div>
         <Container>
@@ -30,25 +31,27 @@ export function SobreMi() {
 
                 <Col md="auto" className='text'>                  
                    <h1 data-aos="fade-up" data-aos-duration="700" data-aos-easing="ease"  data-aos-delay="400"> Sobre Mi</h1>
-                  <p data-aos="fade-up" data-aos-duration="700" data-aos-easing="ease"  data-aos-delay="600">Soy Desarrollador Frontend junior pero mis ganas de aprender y hacer bien mi trabajo son cosas que me motivan cada día para ser mejor y amar lo que hago. <br />Mis conocimientos de programación y desarrollo todavía son limitadas, pero cada día aprendo e investigo sobre las tecnologías más utilizadas y mis ganas de aprender son mayores cada día.</p>
-                  <Button data-aos="fade-up" data-aos-duration="700" data-aos-easing="ease"  data-aos-delay="800" href={CV} target="_blank" rel="noopener noreferrer" download="CV-JhonRengifo.pdf">Descargar CV</Button>
+                  <p data-aos="fade-up" data-aos-duration="300" data-aos-easing="ease"  data-aos-delay="600"> Desarrollador Frontend junior pero mis ganas de aprender y hacer bien mi trabajo son cosas que me motivan cada día para ser mejor y amar lo que hago. <br />Mis conocimientos de programación y desarrollo todavía son limitadas pero mi ambicion de aprender y superarme cada dias son mas grandes</p>
+                  <Button data-aos="fade-up" data-aos-duration="300" data-aos-easing="ease"  data-aos-delay="800" href={CV} target="_blank" rel="noopener noreferrer" download="CV-JhonRengifo.pdf" variant="outline-dark"
+  
+                  >Descargar CV</Button>
 
                   <h2 data-aos="fade-up" data-aos-duration="700" data-aos-easing="ease"  data-aos-delay="1000">Habilidades</h2>
                   <Row>
-                    <h3 data-aos="fade-up" data-aos-duration="700" data-aos-easing="ease"  data-aos-delay="1100">Programacion</h3>
-                      <Col xs={6} md={4}><img data-aos="fade-up" data-aos-duration="700" data-aos-easing="ease"  data-aos-delay="400" src={bash} alt="Bash" /> </Col>
-                      <Col xs={6} md={4}><img data-aos="fade-up" data-aos-duration="700" data-aos-easing="ease"  data-aos-delay="600" src={js} alt="Javascript" /></Col>
-                      <Col xs={6} md={4}><img data-aos="fade-up" data-aos-duration="700" data-aos-easing="ease"  data-aos-delay="800" src={py} alt="Python" /></Col>
-                      <Col xs={6} md={4}><img data-aos="fade-up" data-aos-duration="700" data-aos-easing="ease"  data-aos-delay="1000" src={ptsql} alt="Postgresql" /></Col>
-                      <Col xs={6} md={4}><img data-aos="fade-up" data-aos-duration="700" data-aos-easing="ease"  data-aos-delay="1200" src={reac} alt="React" /></Col>
-                      <Col xs={6} md={4}><img data-aos="fade-up" data-aos-duration="700" data-aos-easing="ease"  data-aos-delay="1400" src={git} alt="Git" /></Col>
-                      <Col xs={6} md={4}><img data-aos="fade-up" data-aos-duration="700" data-aos-easing="ease"  data-aos-delay="1600" src={html} alt="HTML" /></Col>
-                    <h3 data-aos="fade-up" data-aos-duration="700" data-aos-easing="ease"  data-aos-delay="1000" >Diseño</h3>
-                      <Col xs={6} md={4}><img data-aos="fade-up" data-aos-duration="700" data-aos-easing="ease"  data-aos-delay="400" src={ds} alt="3ds Max" /></Col>
-                      <Col xs={6} md={4}><img data-aos="fade-up" data-aos-duration="700" data-aos-easing="ease"  data-aos-delay="600" src={boot} alt="Bootstrap" /></Col>
-                      <Col xs={6} md={4}><img data-aos="fade-up" data-aos-duration="700" data-aos-easing="ease"  data-aos-delay="800" src={css} alt="CSS" /></Col>
-                      <Col xs={6} md={4}><img data-aos="fade-up" data-aos-duration="700" data-aos-easing="ease"  data-aos-delay="1000" src={photo} alt="PhotoShop" /></Col>
-                      <Col xs={6} md={4}><img data-aos="fade-up" data-aos-duration="700" data-aos-easing="ease"  data-aos-delay="1200" src={figma} alt="Figma" /></Col>
+                    <h3 data-aos="fade-up" data-aos-duration="700" data-aos-easing="ease"  data-aos-delay="300">Programacion</h3>
+                      <Col xs={6} md={4}><img data-hint="Bash" data-aos="fade-up" data-aos-duration="400" data-aos-easing="ease"  data-aos-delay="400"  src={bash} alt="Bash" /> </Col>
+                      <Col xs={6} md={4}><img data-aos="fade-up" data-aos-duration="400" data-aos-easing="ease"  data-aos-delay="500" src={js} alt="Javascript" /></Col>
+                      <Col xs={6} md={4}><img data-aos="fade-up" data-aos-duration="400" data-aos-easing="ease"  data-aos-delay="600" src={py} alt="Python" /></Col>
+                      <Col xs={6} md={4}><img data-aos="fade-up" data-aos-duration="400" data-aos-easing="ease"  data-aos-delay="700" src={ptsql} alt="Postgresql" /></Col>
+                      <Col xs={6} md={4}><img data-aos="fade-up" data-aos-duration="400" data-aos-easing="ease"  data-aos-delay="800" src={reac} alt="React" /></Col>
+                      <Col xs={6} md={4}><img data-aos="fade-up" data-aos-duration="400" data-aos-easing="ease"  data-aos-delay="900" src={git} alt="Git" /></Col>
+                      <Col xs={6} md={4}><img data-aos="fade-up" data-aos-duration="400" data-aos-easing="ease"  data-aos-delay="1000" src={html} alt="HTML" /></Col>
+                    <h3 data-aos="fade-up" data-aos-duration="700" data-aos-easing="ease"  data-aos-delay="300" >Diseño</h3>
+                      <Col xs={6} md={4}><img data-aos="fade-up" data-aos-duration="400" data-aos-easing="ease"  data-aos-delay="400" src={ds} alt="3ds Max" /></Col>
+                      <Col xs={6} md={4}><img data-aos="fade-up" data-aos-duration="400" data-aos-easing="ease"  data-aos-delay="500" src={boot} alt="Bootstrap" /></Col>
+                      <Col xs={6} md={4}><img data-aos="fade-up" data-aos-duration="400" data-aos-easing="ease"  data-aos-delay="600" src={css} alt="CSS" /></Col>
+                      <Col xs={6} md={4}><img data-aos="fade-up" data-aos-duration="400" data-aos-easing="ease"  data-aos-delay="700" src={photo} alt="PhotoShop" /></Col>
+                      <Col xs={6} md={4}><img data-aos="fade-up" data-aos-duration="400" data-aos-easing="ease"  data-aos-delay="800" src={figma} alt="Figma" /></Col>
 
                   </Row>
                 </Col>
