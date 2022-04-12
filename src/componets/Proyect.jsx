6ad232ef1a33d4f9f752d9505b52030dda8e1262
unknown <RengifoVera>
2022-04-12 16:36:47 -0500

    Deploy

--- src/componets/Proyect.jsx
+++ src/componets/Proyect.jsx
@@ -1,6 +1,7 @@
 import React ,{useEffect} from 'react'
 import {Row,Col,Container} from 'react-bootstrap';
 import AOS from 'aos';
+import pokedex from './img/pokedex.png'
 
 
 import "./styles/SobreMi.css";
@@ -12,44 +13,18 @@ export function Proyect() {
 
   
   return (
-      <div className='contacto back-color'> 
+      <div className='back-color'> 
         <Container>
           <Row className="justify-content-md-center">
-            <h1 className='text'  data-aos="fade-up"  data-aos-duration="700" data-aos-easing="ease"  data-aos-delay="300"> Proyectos</h1>
-              <Col xs={6} md={4} className='text'  data-aos="fade-up"  data-aos-duration="700" data-aos-easing="ease"  data-aos-delay="400"  ><h1>Moviles</h1> 
-              <Col>
-                  <ul className='practicas_python'>
-                    <li><h6>Working on that</h6></li>
-                  </ul>
+            
+          <h1 data-aos="fade-up" data-aos-duration="700" data-aos-easing="ease" className='text' data-aos-delay="1000">Proyectos</h1>
+                <Col md="auto" className='text img-card'>                  
+                  <Row>
+                    <Col xs={6} md={4}><img data-aos="fade-up" data-aos-duration="400" data-aos-easing="ease"  data-aos-delay="700" src={pokedex} alt="PhotoShop" /></Col>
+                    <Col xs={6} md={4}><img data-aos="fade-up" data-aos-duration="400" data-aos-easing="ease"  data-aos-delay="800" src={pokedex} alt="Figma" /></Col>
+                    <Col xs={6} md={4}><img data-aos="fade-up" data-aos-duration="400" data-aos-easing="ease"  data-aos-delay="800" src={pokedex} alt="Figma" /></Col>
+                  </Row>
                 </Col>
-              </Col>
-              <Col xs={6} md={4} className='text'   data-aos="fade-up"  data-aos-duration="700" data-aos-easing="ease"  data-aos-delay="400">
-                <h1>Web</h1>
-                <Col>
-                  <ul className='practicas_python'>
-                    <li><h6>Working on that</h6></li>
-                  </ul>
-                </Col>
-              </Col>
-              <Col xs={6} md={4} className='text'  data-aos="fade-up"  data-aos-duration="700" data-aos-easing="ease"  data-aos-delay="400" ><h1>Desktop</h1>
-              <Col>
-                  <ul className='practicas_python'>
-                    <li><h6>Working on that</h6></li>
-                  </ul>
-                </Col>
-              </Col>
-              <Col xs={6} md={4} className='text'  data-aos="fade-up"  data-aos-duration="700" data-aos-easing="ease"  data-aos-delay="400">
-                <h1>Code</h1>
-                  <Col>
-                      <ul className='practicas_python'>
-                        <li><a href="https://github.com/RengifoVera/Practicas_Python/blob/main/el_viajero.py" target="_blank"> <h6>Problema Del Viajero</h6></a></li>
-                        <li><a href="https://github.com/RengifoVera/Practicas_Python/blob/main/control_boletas.py" target="_blank"> <h6>Control Boletas</h6></a></li>
-                        <li><a href="https://github.com/RengifoVera/Practicas_Python/blob/main/JuegoDados.py" target="_blank"><h6>Problema de los Dados Cargados</h6></a></li>
-                        <li><a href="https://github.com/RengifoVera/Practicas_Python/blob/main/Tiro_moneda.py" target="_blank"><h6>Tiro Moneda</h6></a></li>
-                        <li><a href="https://github.com/RengifoVera/Practicas_Python/tree/main/Taller%202" target="_blank" ><h6>Pruebas y Generadores </h6></a></li>
-                      </ul>
-                  </Col>
-              </Col>
           </Row>
         </Container>
       </div>
